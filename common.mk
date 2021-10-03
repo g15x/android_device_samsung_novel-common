c446b8431121074e9aa8ca931cd6a9d82db1f66b
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2021-10-03 09:21:58 +0900

    Fix build error

--- common.mk
+++ common.mk
@@ -78,4 +78,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
 # Include non-opensource parts
-$(call inherit-product, vendor/samsung/novel-common/common-vendor.mk)
+$(call inherit-product, vendor/samsung/novel-common/novel-vendor.mk)
