2ba9124b7596990291ea7eed09720dccbd570201
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2022-08-02 11:00:36 +0900

    added stlport package

--- device-common.mk
+++ device-common.mk
@@ -81,6 +81,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Libstlport
+PRODUCT_PACKAGES += \
+    libstlport
+
 # Gralloc
 PRODUCT_PACKAGES += \
     gralloc.exynos3 \
